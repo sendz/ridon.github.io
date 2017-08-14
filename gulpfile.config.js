40c8d9c89430f4ab2929bd535935dde52a06ad6c
sendz <sendzation@gmail.com>
2017-08-14 21:47:55 +0700

    Fix velocity

--- gulpfile.config.js
+++ gulpfile.config.js
@@ -2,9 +2,9 @@ module.exports = {
   css: [],
   js: [
     './source/js/modernizr.custom.32033.js',
-    './source/js/velocity.min.js',
     './source/js/jquery-3.2.1.js',
     './source/js/jquery.easing.1.4.js',
+    './source/js/velocity.min.js',
     './source/js/hammer.min.js',
     './source/js/jquery.hammer.js',
     './source/js/initial.js',
