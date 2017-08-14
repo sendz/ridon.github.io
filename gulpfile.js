8bd06b10579f857d167c41e8ebdcd92da2211b5a
sendz <sendzation@gmail.com>
2017-08-14 22:36:55 +0700

    Add CNAME

--- gulpfile.js
+++ gulpfile.js
@@ -61,6 +61,8 @@ gulp.task('build-deploy', ['build-css', 'build-js', 'copy-fonts', 'copy-images']
     gulp.src('assets/**/*')
       .pipe(gulp.dest('dist/assets')),
     gulp.src('index.html')
+      .pipe(gulp.dest('dist')),
+    gulp.src('CNAME')
       .pipe(gulp.dest('dist'))
   )
 })
