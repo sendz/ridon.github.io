c80fb7d14d68cf1e42427f0c571cc6d932e66843
sendz <sendzation@gmail.com>
2017-08-14 21:26:56 +0700

    Add featured devices

--- gulpfile.config.js
+++ gulpfile.config.js
@@ -2,6 +2,7 @@ module.exports = {
   css: [],
   js: [
     './source/js/modernizr.custom.32033.js',
+    './source/js/velocity.min.js',
     './source/js/jquery-3.2.1.js',
     './source/js/jquery.easing.1.4.js',
     './source/js/hammer.min.js',
@@ -14,6 +15,9 @@ module.exports = {
     './source/js/parallax.js',
     './source/js/scrollspy.js',
     './source/js/slider.js',
+    './source/js/waves.js',
+    './source/js/tabs.js',
+    './source/js/waves.js',
     './source/js/script.js'
   ]
 }
