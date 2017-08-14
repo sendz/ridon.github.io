7234d85e139a58d1500c95af6bd61adf4f72ece3
sendz <sendzation@gmail.com>
2017-08-14 22:13:03 +0700

    Try and error

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var deploy = require('gulp-gh-pages')
 gulp.task('default', ['watch', 'copy-fonts', 'copy-images', 'build-js'])
 
 gulp.task('deploy', ['build-css', 'build-js', 'copy-fonts', 'copy-images'], function () {
-  return gulp.src('.')
+  return gulp.src(['assets/**/*', 'source/**/*', 'index.html', '!node_modules', '!.git'])
     .pipe(deploy({origin: 'origin', branch: 'gh-pages'}))
 })
 
