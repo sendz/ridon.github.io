d865595f21f58838a45c0de0e4706e4f0a2538d8
sendz <sendzation@gmail.com>
2017-08-14 21:38:07 +0700

    Change to remoteUrl

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('default', ['watch', 'copy-fonts', 'copy-images', 'build-js'])
 
 gulp.task('deploy', ['build-css', 'build-js', 'copy-fonts', 'copy-images'], function () {
   return gulp.src('assets/**/*')
-    .pipe(deploy({origin: 'upstream', branch: 'master'}))
+    .pipe(deploy({remoteUrl: 'git@github.com:ridon/ridon.github.io.git', branch: 'master'}))
 })
 
 gulp.task('jshint', function() {
